5a1cf3b841d91095cd6f6fe6b818573ba35b6669
Simone Rizza <simonerizza@macbookpro.home>
2025-01-04 00:41:27 +0100

    adding dist

--- dist/assets/FolioButton.01966248.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,I as o,J as s,V as n,O as _}from"./index.cdea055a.js";const a={name:"FolioButton"},r={class:"open-button"},c=_("Open");function p(e,u,d,l,i,f){return o(),s("button",r,[n(e.$slots,"default",{},()=>[c],!0)])}var v=t(a,[["render",p],["__scopeId","data-v-43733ee6"]]);export{v as F};
