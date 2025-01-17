1113848cdef0a4955fe154a178212c17bb03a108
Simone Rizza <simonerizza@macbookpro.home>
2025-01-17 15:12:29 +0100

    feat(routing): wip

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,27 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 
-export default defineConfig({
-  base: '/portfolio-vite/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import('vite-plugin-static-copy');
+
+  return {
+    base: '/portfolio-vite/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [vue()],
+    plugins: [
+      vue(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'dist/index.html',
+            dest: '.',
+            rename: '404.html',
+          },
+        ],
+      }),
+    ],
+  };
 });
